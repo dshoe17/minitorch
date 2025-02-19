efcf2b11d45eb7aa525c4221f24dcc4f0684489f
dshoe17 <dshoe97@gmail.com>
2025-02-19 16:34:51 -0700

    fixed typo in lt

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -32,7 +32,7 @@ def neg(x: float) -> float:
 
 def lt(x: float, y: float) -> float:
     "$f(x) =$ 1.0 if x is less than y else 0.0"
-    return -1.0 if x < y else 0.0
+    return 1.0 if x < y else 0.0
 
 
 def eq(x: float, y: float) -> float:
